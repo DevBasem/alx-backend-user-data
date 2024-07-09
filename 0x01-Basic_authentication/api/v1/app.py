915f8313b79d06c579883c92fbc7b9bb5c37e51a
DevBasem <engr.basemahmed@gmail.com>
2024-07-09 10:50:27 -0700

    update 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,7 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS, cross_origin
 import os
 
 app = Flask(__name__)
@@ -19,7 +19,9 @@ auth_type = getenv('AUTH_TYPE')
 if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-
+elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -27,20 +29,17 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
-
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
 
 @app.before_request
 def before_request_func():
@@ -53,7 +52,6 @@ def before_request_func():
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
     ]
-
     if not auth.require_auth(request.path, excluded_paths):
         return
 
@@ -62,7 +60,6 @@ def before_request_func():
 
     if auth.current_user(request) is None:
         abort(403)
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+BasicAuth module for the API
+"""
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth
+    """
+    pass
