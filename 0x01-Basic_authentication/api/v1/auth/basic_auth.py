cab259c699ced6666168a734479e37e01bffee12
DevBasem <engr.basemahmed@gmail.com>
2024-07-09 11:03:54 -0700

    task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@ BasicAuth module for the API
 """
 from api.v1.auth.auth import Auth
 
+import base64
+
 
 class BasicAuth(Auth):
     """
@@ -33,3 +35,30 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(
+        self, base64_authorization_header: str
+    ) -> str:
+        """
+        Decodes a Base64 encoded string
+
+        Args:
+            base64_authorization_header (str): The Base64
+            encoded string
+
+        Returns:
+            str: The decoded string in UTF-8, or None
+            if decoding fails
+        """
+        if (
+            base64_authorization_header is None or
+            not isinstance(base64_authorization_header, str)
+        ):
+            return None
+        
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_str = decoded_bytes.decode('utf-8')
+            return decoded_str
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
