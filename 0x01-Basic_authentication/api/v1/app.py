7f62f56c27157f58597efd52e21730d728e441df
DevBasem <engr.basemahmed@gmail.com>
2024-07-09 07:47:06 -0700

    task 3

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -32,6 +32,7 @@ def forbidden_error(error):
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
