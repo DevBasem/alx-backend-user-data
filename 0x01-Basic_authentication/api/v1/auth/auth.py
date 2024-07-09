8d101ff83684c8a33ced4691a7f0e20766069584
DevBasem <engr.basemahmed@gmail.com>
2024-07-09 12:08:49 -0700

    fixes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,35 +5,47 @@ Auth module for API authentication
 
 from flask import request
 from typing import List, TypeVar
-import fnmatch
 
 
 class Auth:
-    """Authentication class.
-    """
+    """ Class to manage the API authentication """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required.
-        """
-        if path is None:
+        """ Method for validating if endpoint requires auth """
+        if path is None or excluded_paths is None or excluded_paths == []:
             return True
 
-        if excluded_paths is None or not excluded_paths:
+        l_path = len(path)
+        if l_path == 0:
             return True
 
-        for excluded_path in excluded_paths:
-            if fnmatch.fnmatch(path, excluded_path):
-                return False
+        slash_path = True if path[l_path - 1] == '/' else False
+
+        tmp_path = path
+        if not slash_path:
+            tmp_path += '/'
+
+        for exc in excluded_paths:
+            l_exc = len(exc)
+            if l_exc == 0:
+                continue
+
+            if exc[l_exc - 1] != '*':
+                if tmp_path == exc:
+                    return False
+            else:
+                if exc[:-1] == path[:l_exc - 1]:
+                    return False
 
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get authorization header.
-        """
-        if request is not None:
-            return request.headers.get('Authorization', None)
-        return None
+        """ Method that handles authorization header """
+        if request is None:
+            return None
+
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get user from request.
-        """
+        """ Validates current user """
         return None
