f36387417cbfa9092dd76015ede88778d1af5e36
DevBasem <engr.basemahmed@gmail.com>
2024-07-09 11:12:32 -0700

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,12 +6,13 @@ from api.v1.auth.auth import Auth
 
 import base64
 
+from models.user import User
+
 
 class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
-
     def extract_base64_authorization_header(
         self, authorization_header: str
     ) -> str:
@@ -55,7 +56,7 @@ class BasicAuth(Auth):
             not isinstance(base64_authorization_header, str)
         ):
             return None
-        
+
         try:
             decoded_bytes = base64.b64decode(base64_authorization_header)
             decoded_str = decoded_bytes.decode('utf-8')
@@ -84,6 +85,38 @@ class BasicAuth(Auth):
             ':' not in decoded_base64_authorization_header
         ):
             return None, None
-        
+
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+        self, user_email: str, user_pwd: str
+    ) -> User:
+        """
+        Returns the User instance based on email and password
+
+        Args:
+            user_email (str): User's email
+            user_pwd (str): User's password
+
+        Returns:
+            User: The User instance if credentials are valid,
+            None otherwise
+        """
+        if (
+            user_email is None or not isinstance(user_email, str) or
+            user_pwd is None or not isinstance(user_pwd, str)
+        ):
+            return None
+
+        users = User.search({'email': user_email})
+
+        if not users:
+            return None
+
+        user = users[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
