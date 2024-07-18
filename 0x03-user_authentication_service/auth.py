b0272e89d2ac53fb80cfdabc16aaceffa14b1ad9
DevBasem <engr.basemahmed@gmail.com>
2024-07-18 02:13:53 -0700

    task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt.hashpw
+    """
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
