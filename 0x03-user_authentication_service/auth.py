525b6304034023159e20dcc6658016a9caf19669
DevBasem <engr.basemahmed@gmail.com>
2024-07-18 02:17:49 -0700

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,13 +2,38 @@
 """
 Auth module
 """
-import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Hashes a password using bcrypt.hashpw
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    # Generate a salt and hash the password
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a new user if email does not exist,
+        otherwise raise ValueError
+        """
+        try:
+            # Check if the user already exists
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # Hash the password
+            hashed_password = self._hash_password(password)
+            # Add the user to the database
+            user = self._db.add_user(email, hashed_password)
+            return user
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes a password using bcrypt.hashpw
+        """
+        import bcrypt
+        # Generate a salt and hash the password
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
